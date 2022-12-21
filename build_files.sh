262a1936938c3a6e708e1b91f7762cf18cdf6566
Umst gh <mohmedalhkim@gmail.com>
2022-12-21 13:53:56 +0200

    fourth commit

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,6 @@
 # build_files.sh
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+echo "Build started"
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+echo "Build Ended"
+
