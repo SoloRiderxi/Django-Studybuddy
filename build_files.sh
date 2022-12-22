5fb2be32b21a4cd7c7367ba0960012b7fc5fdad9
Umst gh <mohmedalhkim@gmail.com>
2022-12-22 11:35:06 +0200

    08 commit

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 # build_files.sh
 echo "Build started"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python3.11 -m pip install -r requirements.txt
+python3.11 manage.py collectstatic --noinput --clear
 echo "Build Ended"
 
