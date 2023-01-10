fb70319ef35a5d3292cda4fb7d015e8eb35abcb4
Umst gh <mohmedalhkim@gmail.com>
2023-01-10 09:58:34 +0200

    vercel 11

--- studybud/settings.py
+++ studybud/settings.py
@@ -170,7 +170,7 @@ MEDIA_URL = 'images/'
 # ]
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_ROOT = BASE_DIR / 'static/images/usersimages'
 
